087de109262ee0e38ccf5a8c5416cd8bde4014cc
Oliver Steiner <oliver@mollo.ch>
2021-02-07 23:23:07 +0100

    Mollo GraphQl now extended

--- mollo_creator_graphql/src/Plugin/GraphQL/Schema/MolloCreatorSchema.php
+++ mollo_creator_graphql/src/Plugin/GraphQL/Schema/MolloCreatorSchema.php
@@ -2,66 +2,17 @@
 
 namespace Drupal\mollo_creator_graphql\Plugin\GraphQL\Schema;
 
-use Drupal\graphql\GraphQL\ResolverBuilder;
-use Drupal\graphql\GraphQL\ResolverRegistry;
-use Drupal\graphql\Plugin\GraphQL\Schema\SdlSchemaPluginBase;
-use Drupal\mollo_creator_graphql\Plugin\GraphQL\Wrapper\MolloConnection;
+use Drupal\graphql\Plugin\GraphQL\Schema\ComposableSchema;
 
 /**
  * @Schema(
  *   id = "mollo",
- *   name = "Mollo Base",
+ *   name = "Mollo Base Schema",
+ *   extensions = "mollo",
  * )
  */
-class MolloCreatorSchema extends SdlSchemaPluginBase {
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getResolverRegistry() {
-    $builder = new ResolverBuilder();
-    $registry = new ResolverRegistry();
-
-    $this->addQueryFields($registry, $builder);
-    $this->addMolloCreatorFields($registry, $builder);
+class MolloCreatorSchema extends ComposableSchema {
 
 
-    return $registry;
-  }
-
-
-  /**
-   * @param \Drupal\graphql\GraphQL\ResolverRegistry $registry
-   * @param \Drupal\graphql\GraphQL\ResolverBuilder $builder
-   */
-  protected function addMolloCreatorFields(ResolverRegistry $registry, ResolverBuilder $builder) {
-
-    $registry->addFieldResolver('MolloInfo', 'version',
-      $builder->callback(function () {
-        return '1.0.1';
-      })
-    );
-    $registry->addFieldResolver('MolloInfo', 'description',
-      $builder->callback(function () {
-        return 'Base GraphQL for Mollo Modules';
-      })
-    );
-
-  }
-
-  /**
-   * @param \Drupal\graphql\GraphQL\ResolverRegistry $registry
-   * @param \Drupal\graphql\GraphQL\ResolverBuilder $builder
-   */
-  protected function addQueryFields(ResolverRegistry $registry, ResolverBuilder $builder): void {
-
-    // Query - mollo info 4
-    $registry->addFieldResolver('Query', 'mollo',
-      $builder->callback(function () {
-        return '';
-      }));
-
-
-  }
 
 }
---
+++ mollo_creator_graphql/src/Plugin/GraphQL/SchemaExtension/MolloCreatorExtension.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Drupal\mollo_creator_graphql\Plugin\GraphQL\SchemaExtension;
+
+use Drupal\graphql\GraphQL\ResolverBuilder;
+use Drupal\graphql\GraphQL\ResolverRegistry;
+use Drupal\graphql\GraphQL\ResolverRegistryInterface;
+use Drupal\graphql\GraphQL\Response\ResponseInterface;
+use Drupal\graphql\Plugin\GraphQL\SchemaExtension\SdlSchemaExtensionPluginBase;
+use Drupal\graphql_composable\GraphQL\Response\ArticleResponse;
+use Drupal\mollo_blog_graphql\Plugin\GraphQL\Response\MolloBlogResponse;
+use Drupal\mollo_creator_graphql\Plugin\GraphQL\Response\MolloInfoResponse;
+use Exception;
+
+/**
+ * @SchemaExtension(
+ *   id = "mollo_extension",
+ *   name = "Mollo  Extension",
+ *   description = "Mollo Extension",
+ *   schema = "mollo"
+ * )
+ */
+class MolloCreatorExtension extends SdlSchemaExtensionPluginBase {
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function registerResolvers(ResolverRegistryInterface $registry) {
+    $builder = new ResolverBuilder();
+
+    $registry->addFieldResolver('MolloInfo', 'version',
+      $builder->callback(function () {
+        return '1.0.1';
+      })
+    );
+    $registry->addFieldResolver('MolloInfo', 'description',
+      $builder->callback(function () {
+        return 'Base GraphQL for Mollo Modules';
+      })
+    );
+
+    // Query - mollo info 4
+    $registry->addFieldResolver('Query', 'mollo',
+      $builder->callback(function () {
+        return '';
+      }));
+  }
+
+  /**
+   * Resolves the response type.
+   *
+   * @param \Drupal\graphql\GraphQL\Response\ResponseInterface $response
+   *   Response object.
+   *
+   * @return string
+   *   Response type.
+   *
+   * @throws \Exception
+   *   Invalid response type.
+   */
+  public static function resolveResponse(ResponseInterface $response): string {
+    // Resolve content response.
+    if ($response instanceof MolloInfoResponse) {
+      return 'MolloResponse';
+    }
+    throw new \Exception('Invalid response type.');
+  }
+
+}
+
